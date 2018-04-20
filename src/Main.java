43d1c75085f3236aaf96609f6757dbab72bcb99b
Lucas Ulitze <Lucas.Ulitze@googlemail.com>
2018-04-20 21:09:57 +0200

    Fertsch

--- src/Main.java
+++ src/Main.java
@@ -97,12 +97,116 @@ public class Main {
 		btnNeu.setBounds(10, 11, 145, 23);
 		frmHangman.getContentPane().add(btnNeu);
 		
-		JButton btnNewButton = new JButton("A");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnA = new JButton("A");
+		btnA.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnNewButton.setBounds(26, 307, 42, 42);
-		frmHangman.getContentPane().add(btnNewButton);
+		btnA.setBounds(26, 307, 42, 42);
+		frmHangman.getContentPane().add(btnA);
+		
+		JButton btnB = new JButton("B");
+		btnB.setBounds(67, 307, 42, 42);
+		frmHangman.getContentPane().add(btnB);
+		
+		JButton btnC = new JButton("C");
+		btnC.setBounds(108, 307, 42, 42);
+		frmHangman.getContentPane().add(btnC);
+		
+		JButton btnD = new JButton("D");
+		btnD.setBounds(149, 307, 42, 42);
+		frmHangman.getContentPane().add(btnD);
+		
+		JButton btnE = new JButton("E");
+		btnE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnE.setBounds(190, 307, 42, 42);
+		frmHangman.getContentPane().add(btnE);
+		
+		JButton btnF = new JButton("F");
+		btnF.setBounds(231, 307, 42, 42);
+		frmHangman.getContentPane().add(btnF);
+		
+		JButton btnG = new JButton("G");
+		btnG.setBounds(272, 307, 42, 42);
+		frmHangman.getContentPane().add(btnG);
+		
+		JButton btnH = new JButton("H");
+		btnH.setBounds(313, 307, 42, 42);
+		frmHangman.getContentPane().add(btnH);
+		
+		JButton btnI = new JButton("I");
+		btnI.setBounds(354, 307, 42, 42);
+		frmHangman.getContentPane().add(btnI);
+		
+		JButton btnJ = new JButton("J");
+		btnJ.setBounds(395, 307, 42, 42);
+		frmHangman.getContentPane().add(btnJ);
+		
+		JButton btnK = new JButton("K");
+		btnK.setBounds(26, 349, 42, 42);
+		frmHangman.getContentPane().add(btnK);
+		
+		JButton btnL = new JButton("L");
+		btnL.setBounds(108, 349, 42, 42);
+		frmHangman.getContentPane().add(btnL);
+		
+		JButton btnM = new JButton("M");
+		btnM.setBounds(67, 349, 42, 42);
+		frmHangman.getContentPane().add(btnM);
+		
+		JButton btnN = new JButton("N");
+		btnN.setBounds(313, 349, 42, 42);
+		frmHangman.getContentPane().add(btnN);
+		
+		JButton btnO = new JButton("O");
+		btnO.setBounds(354, 349, 42, 42);
+		frmHangman.getContentPane().add(btnO);
+		
+		JButton btnP = new JButton("P");
+		btnP.setBounds(395, 349, 42, 42);
+		frmHangman.getContentPane().add(btnP);
+		
+		JButton btnQ = new JButton("Q");
+		btnQ.setBounds(149, 390, 42, 42);
+		frmHangman.getContentPane().add(btnQ);
+		
+		JButton btnR = new JButton("R");
+		btnR.setBounds(26, 390, 42, 42);
+		frmHangman.getContentPane().add(btnR);
+		
+		JButton btnS = new JButton("S");
+		btnS.setBounds(108, 390, 42, 42);
+		frmHangman.getContentPane().add(btnS);
+		
+		JButton btnT = new JButton("T");
+		btnT.setBounds(67, 390, 42, 42);
+		frmHangman.getContentPane().add(btnT);
+		
+		JButton btnU = new JButton("U");
+		btnU.setBounds(190, 390, 42, 42);
+		frmHangman.getContentPane().add(btnU);
+		
+		JButton btnV = new JButton("V");
+		btnV.setBounds(231, 390, 42, 42);
+		frmHangman.getContentPane().add(btnV);
+		
+		JButton btnW = new JButton("W");
+		btnW.setBounds(313, 390, 42, 42);
+		frmHangman.getContentPane().add(btnW);
+		
+		JButton btnX = new JButton("X");
+		btnX.setBounds(272, 390, 42, 42);
+		frmHangman.getContentPane().add(btnX);
+		
+		JButton btnY = new JButton("Y");
+		btnY.setBounds(354, 390, 42, 42);
+		frmHangman.getContentPane().add(btnY);
+		
+		JButton btnZ = new JButton("Z");
+		btnZ.setBounds(395, 390, 42, 42);
+		frmHangman.getContentPane().add(btnZ);
 	}
 }
